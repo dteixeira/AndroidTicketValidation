5d07ea5df03115cdd690c072b75fa7dcec55a4c7
Diogo Teixeira <diogo.andre.teixeira@gmail.com>
2013-10-27 18:43:59 +0000

    Unused ticket list added.

--- TicketClient/src/org/cmov/ticketclient/SectionsPagerAdapter.java
+++ TicketClient/src/org/cmov/ticketclient/SectionsPagerAdapter.java
@@ -1,18 +1,31 @@
 package org.cmov.ticketclient;
 
+import java.io.Serializable;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
-public class SectionsPagerAdapter extends FragmentPagerAdapter {
+public class SectionsPagerAdapter extends FragmentPagerAdapter implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	private Activity context = null;
+	private Fragment mUnusedTicketsFragment = null;
+
+	public Fragment getmUnusedTicketsFragment() {
+		return mUnusedTicketsFragment;
+	}
+	
+	public void setContext(Activity context) {
+		this.context = context;
+	}
 
 	public SectionsPagerAdapter(FragmentManager fm, Activity context) {
 		super(fm);
 		this.context = context;
+		mUnusedTicketsFragment = new TicketUnusedFragment();
 	}
 
 	@Override
@@ -23,11 +36,20 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 		Bundle bundle = null;
 		switch (position) {
 		case 0:
-			fragment = new TicketListFragment();
+			bundle = new Bundle();
+			mUnusedTicketsFragment.setArguments(bundle);
+			return mUnusedTicketsFragment;
+		case 1:
+			fragment = new TicketUsedFragment();
 			bundle = new Bundle();
 			fragment.setArguments(bundle);
 			return fragment;
-		case 1:
+		case 2:
+			fragment = new TicketPresentFragment();
+			bundle = new Bundle();
+			fragment.setArguments(bundle);
+			return fragment;
+		case 3:
 			fragment = new TicketBuyFragment();
 			bundle = new Bundle();
 			fragment.setArguments(bundle);
@@ -39,15 +61,19 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
 	@Override
 	public int getCount() {
-		return 2;
+		return 4;
 	}
 
 	@Override
 	public CharSequence getPageTitle(int position) {
 		switch (position) {
 		case 0:
-			return context.getString(R.string.title_list_tickets);
+			return context.getString(R.string.title_unused_tickets);
 		case 1:
+			return context.getString(R.string.title_used_tickets);
+		case 2:
+			return context.getString(R.string.title_present_ticket);
+		case 3:
 			return context.getString(R.string.title_buy_tickets);
 		}
 		return null;
